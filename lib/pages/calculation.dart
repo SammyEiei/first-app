d1d77057e92eeb8f6bccf6599b63560169a4c61b
SammyEiei <wasanrenumat@Wasans-MacBook-Pro-7.local>
2025-07-14 10:28:13 +0700

    fix: resolve BuildContext undefined error by ensuring flutter dependencies

---
+++ lib/pages/calculation.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/widgets.dart';
+
+class CalculationPage extends StatefulWidget {
+  const CalculationPage({ Key? key}): super(key: key);
+  @override
+  _CalculationPageState createState() => _CalculationPageState();
+
+}
+class _CalculationPageState extends State<CalculationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+}
+}
+
